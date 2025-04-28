eb70c6e8e36f3bb334ada894115fa9cb2c87e814
Dave <kreeckinc@gmail.com>
2025-04-28 18:55:33 +0200

    creating the blog and the contact application

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,12 +12,10 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    # Add authentication URLs (login, logout, password reset)
-    path(
-        "", include("django.contrib.auth.urls")
-    ),  # This includes the default auth URLs (login, logout, password reset, etc.)
+    path("", include("django.contrib.auth.urls")),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+
     # User management URLs
     path("register/", views.register_user, name="register_user"),
     path("update/<int:user_id>/", views.update_user, name="update_user"),
@@ -27,17 +25,8 @@ urlpatterns = [
     path("manage_roles/", views.manage_roles, name="manage_roles"),
     path("manage_departments/", views.manage_departments, name="manage_departments"),
     # Password reset views
-    path(
-        "password-reset/",
-        PasswordResetView.as_view(template_name="registration/password_reset.html"),
-        name="password_reset",
-    ),
-    path(
-        "password-reset/done/",
-        PasswordResetDoneView.as_view(
-            template_name="registration/password_reset_done.html"
-        ),
-        name="password_reset_done",
+    path("password-reset/", PasswordResetView.as_view(template_name="registration/password_reset.html"), name="password_reset",),
+    path("password-reset/done/",PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"),name="password_reset_done",
     ),
     path(
         "password-reset-confirm/<uidb64>/<token>/",
