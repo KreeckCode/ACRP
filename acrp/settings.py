881467002625b825f067aa4410e62d5242a5ed3a
Dave Macbook <kreeckinc@gmail.com>
2025-07-16 10:29:05 +0200

    adding static files

--- acrp/settings.py
+++ acrp/settings.py
@@ -113,16 +113,11 @@ DEBUG_TOOLBAR_CONFIG = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': config('DB_NAME'),
-            'USER': config('DB_USER'),
-            'PASSWORD': config('DB_PASS'),
-            'HOST': config('DB_HOST'), 
-            'PORT': config('DB_PORT'),  
-        }
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-
+}
 
 
 # Password validation
