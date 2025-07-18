d89b5095414ef7cc2fbcbd9a7673b01d44a259d3
Dave Macbook <kreeckinc@gmail.com>
2025-07-18 09:34:18 +0200

    deleting dead code - removing  redundent apps to reduce complexity

--- acrp/settings.py
+++ acrp/settings.py
@@ -51,15 +51,8 @@ THIRED_PARTY_APPS = [
 
 # Custom apps
 PROJECT_APPS = [
-    'common',
     'app',
     'accounts',
-    'finance',
-    'hr',
-    'database',
-    'chat',
-    'providers',
-    'student',
     'enrollments',
 ]
 
@@ -113,11 +106,16 @@ DEBUG_TOOLBAR_CONFIG = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASS'),
+            'HOST': config('DB_HOST'), 
+            'PORT': config('DB_PORT'),  
+        }
     }
-}
+
 
 
 # Password validation
